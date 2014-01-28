9d33f7b40d87ccbcc8f970eb1fe84c1eead36a5d
Jake Zerrer <him@jakezerrer.com>
2014-01-28 10:27:50 -0500

    Started testing dateDisplay

---
+++ test/DirectivesSpec.js
@@ -0,0 +1,99 @@
+'use strict';
+ 
+describe('Directive: dateDisplay', function(){
+    var scope,
+      elem,
+      compiled,
+      mockTime,
+      evalTime;
+
+     beforeEach(function (){
+    //load the module
+    module('Om.directives');
+    
+    //set our view html.
+    
+    inject(function($compile, $rootScope) {
+      scope = $rootScope.$new();
+
+      mockTime = function(time) {
+        return '<div date-display timestamp=' + time + '></div>';    
+        };
+      
+      evalTime = function(mockTime) {
+          //get the jqLite or jQuery element
+          elem = angular.element(mockTime);
+          
+          //compile the element into a function to 
+          // process the view.
+          compiled = $compile(elem);
+          
+          //run the compiled view.
+          compiled(scope);
+          
+          //call digest on the scope!
+          scope.$digest();        
+          };
+
+    });
+  });
+
+    it('should display "moments ago" if timestamp is less than 60 seconds old.', function() {
+        //check to see if it's blank first.
+
+        var time = mockTime(Date.now());
+
+        evalTime(time);
+
+        expect(elem.text()).toBe('just now');
+
+    });
+
+    it('should display "[n] minutes ago" if the timestamp is 90 seconds old.', function() {
+        // Subtract the time, don't add it!
+        var time = mockTime(Date.now() - 90000);
+
+        evalTime(time);
+
+        expect(elem.text()).toBe('1 minute ago');
+
+        });
+
+    describe('Minutes: 2m <= t <= 60m', function() {
+
+       it('lower boundry', function() {
+           var time = mockTime(Date.now() - 150000);
+
+           evalTime(time);
+
+           expect(elem.text()).toBe('2 minutes ago');
+       });
+
+       it('upper boundry', function() {
+           var time = mockTime(Date.now() - 3570000);
+
+           evalTime(time);
+
+           expect(elem.text()).toBe('59 minutes ago');
+
+       });
+
+    });
+
+    describe('Hour: 1h1m <= t <= 1h59m', function() {
+        
+    });
+
+    describe('Hours until yesterday: 2h1m <= t <= 2am', function() {
+
+    });
+
+    describe('Day: 2am <= previous 2am', function() {
+
+    });
+
+    describe('Days: 2 days ago <= t <= previous 31st of the month', function() {
+
+    })
+
+});
