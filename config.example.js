ecdf07ebdcb426d00311a8aa47779bcc6fded77f
Jake Zerrer <him@jakezerrer.com>
2014-02-06 14:15:26 -0500

    Clarified Cloudant authorization process

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 angular.module('Om').constant('POUCHDB_CONFIG', {
     // Database name:
-    remote: 'https://username.cloudant.com/om/'
+    remote: 'https://username:password@username.cloudant.com/om/'
 });
