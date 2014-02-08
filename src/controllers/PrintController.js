a414510a5d3fa9b4c701f19edeb0d2830580ba42
Jake Zerrer <him@jakezerrer.com>
2014-02-08 16:05:37 -0500

    Created print view

--- src/controllers/PrintController.js
+++ src/controllers/PrintController.js
@@ -1,5 +1,8 @@
 angular.module('Om.controllers')
     .controller('PrintController', ['$scope', 'database', function($scope, database) {
-
+        $scope.actions = function() {
+            // Filtering by action type should happen here
+            return database.container();
+        }
     }]);
 
